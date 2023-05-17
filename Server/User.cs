270d211637f35a1776036868e9c0d895b6c76353
규태 박 <pkt45349048@gmail.com>
2023-05-18 05:29:28 +0900

    함수간 매개변수로 주고받기 위한 User 인터페이스와 클래스

--- Server/User.cs
+++ Server/User.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,74 +6,7 @@ using System.Threading.Tasks;
 
 namespace Server
 {
-    internal interface User
+    internal class User
     {
-        void SetStuID(string stuID);
-        void SetPwd(String pwd);
-        void SetCourseID(String ci);
-        void SetIdx(short idx);
-        void SetVar(String var);
-
-
-        String GetStuID();
-        String GetPwd();
-        String GetCourseID();
-        short GetIdx();
-        String GetVar();
-
-    }
-
-
-    public class user : User
-    {
-        String stuid;
-        String pwd;
-        String ci;
-        short idx;
-        String var;
-
-
-        public void SetStuID(string stuID)
-        {
-            this.stuid = stuID;
-        }
-        public void SetPwd(String pwd)
-        {
-            this.pwd = pwd;
-        }
-        public void SetCourseID(String ci)
-        {
-            this.ci = ci;
-        }
-         public void SetIdx(short idx)
-        {
-            this.idx = idx;
-        }
-        public void SetVar(String var)
-        {
-            this.var = var;
-        }
-
-
-        public String GetStuID()
-        {
-            return stuid;
-        }
-        public String GetPwd()
-        {
-            return pwd;
-        }
-        public String GetCourseID()
-        {
-            return ci;
-        }
-        public short GetIdx()
-        {
-            return idx;
-        }
-        public String GetVar()
-        {
-            return var;
-        }
     }
 }
