7d455da83d7d9e64b558115473ca06ecbeee365d
GYUTAE PARK <64678476+doraemon500@users.noreply.github.com>
2023-05-18 05:13:50 +0900

    Packet 세부 동작 추가와 관련 주석 추가

--- Server/Packet.cs
+++ Server/Packet.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
+using System.Data;
 
 namespace Server
 {
@@ -21,29 +22,57 @@ namespace Server
         TimeConflicts = 3, //시간이 겹침
         AlreadyRegistered = 4, // 이미 신청한 과목을 또 신청하려고 할 때
         OverCapacity = 5, //과목조회에는 성공했으나 신청을 누르기 전에 만석 <- '수강인원이 초과되어...' 에 대응
+        Error = 6
     }
 
     public enum InquireResult
     {
-        OK = 0, //조회 성공
-        WrongCourseNumber = 1, //잘못된 학정번호
-        AlreadyTaken = 2, //이미 수강한 과목 신청시도(재수강불가)
-        AlreadyFull = 3, //과목조회 단계에서 만석 <- '만석입니다' 에 대응
+        OK = 7, //조회 성공
+        WrongCourseNumber = 8, //잘못된 학정번호
+        AlreadyTaken = 9, //이미 수강한 과목 신청시도(재수강불가)
+        AlreadyFull = 10, //과목조회 단계에서 만석 <- '만석입니다' 에 대응
+        Error = 11
     }
 
     public enum LoginResult
     {
-        OK = 0,
-        WrongPassword = 1,
-        NotYourDate = 2,
-        ServerOff = 3
+        OK = 12,
+        WrongPassword = 13,
+        NotYourDate = 14,
+        ServerOff = 15
     }
 
     public enum FavoritesResult
     {
-        OK = 0,
-        AlreadyExist = 1 //선택한 즐겨찾기 번호에 이미 다른과목이 있는 경우
+        OK = 16,
+        FAIL=17,
+        AlreadyExist = 18 //선택한 즐겨찾기 번호에 이미 다른과목이 있는 경우
     }
+
+    public enum First_ProcessResult
+    {
+        OK = 19,
+        Error = 20
+    }
+
+    public enum Packet_Type
+    {
+        GoLogin = 0, // DBLogin()
+
+        GetFavoirtes = 1, // InquireFavorites()
+        AddToFavorites = 2, // AddToFavorites()
+        DeleteFromFavorites = 4, // DeleteFromFavorites()
+
+        GoRegister = 5, // RegisterCourse()
+        GoInquire = 6, // InquireCourse()
+        GetRegisterCourses = 7, // GetMyRegisteredList()
+        DropCourse = 8, // DropCourse()
+        SearchCouse = 9, // SearchCouse()
+
+        GetTypes = 10,
+        GetDepartments = 11
+    }
+
 
     public enum PacketSendERROR
     {
@@ -67,20 +96,21 @@ namespace Server
         {
             MemoryStream ms = new MemoryStream(1024 * 4); // OS DATA CONTROL BASIC SIZE 4KB
             BinaryFormatter bf = new BinaryFormatter();
-            bf.Serialize(ms, 0);
+            bf.Serialize(ms, o);
             return ms.ToArray();
         }
 
         public static Object Desserialize(byte[] bt)
         {
             MemoryStream ms = new MemoryStream(1024 * 4); // OS DATA CONTROL BASIC SIZE 4KB
-            foreach( byte b in bt)
+            foreach (byte b in bt)
             {
                 ms.WriteByte(b);
             }
 
             ms.Position = 0;
             BinaryFormatter bf = new BinaryFormatter();
+            bf.Binder = new AllowAllAssembly();
             Object obj = bf.Deserialize(ms);
             ms.Close();
             return obj;
@@ -91,17 +121,55 @@ namespace Server
     [Serializable]
     public class Initialize : Packet
     {
-        public int Data = 0;
+        public string stuID;
+        public DataSet ds;
     }
 
     [Serializable]
     public class Login : Packet
     {
-        public string m_strID;
+        public string stuID;
+        public string pwd;
+        public DataSet ds;
+    }
 
-        public Login()
+    [Serializable]
+    public class Register : Packet
+    {
+        public string stuID;
+        public string ci;
+        public string var;
+        public DataSet ds;
+    }
+
+    [Serializable]
+    public class inquire : Packet
+    {
+        public string stuID;
+        public string ci;
+        public DataSet ds;
+    }
+
+    [Serializable]
+    public class Favorites : Packet
+    {
+        public string stuID;
+        public string ci;
+        public short idx;
+        public DataSet ds;
+    }
+
+    // Serializationexception : Cant find Assembly 해결용
+    sealed class AllowAllAssembly : System.Runtime.Serialization.SerializationBinder
+    {
+        public override Type BindToType(string assemblyName, string typeName)
         {
+            Type type = null;
 
+            String currentAssem = System.Reflection.Assembly.GetExecutingAssembly().FullName;
+            type = Type.GetType(String.Format("{0},{1}", typeName, currentAssem));
+
+            return type;
         }
     }
 }
