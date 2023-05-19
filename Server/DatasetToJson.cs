545185cbd99ad2292d77db9f1e5b794b8f5e47d8
star1502 <yunasin2442@gmail.com>
2023-05-19 13:22:57 +0900

    누락된 JSON 직렬화 파일 추가

---
+++ Server/DatasetToJson.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using MySqlX.XDevAPI.Relational;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Server
+{
+    //사용을 위해서는 Package 관리자에서
+    //Install-Package Newtonsoft.Json
+    //입력
+    internal class DatasetToJson
+    {
+        public static string SerializeToJSON(DataSet argDs)
+        {
+            string JSONString = string.Empty;
+            JSONString = JsonConvert.SerializeObject(argDs);
+            return JSONString;
+        }
+
+        //public DataSet DeSerializeFromJSON(string arg)
+    }
+}
